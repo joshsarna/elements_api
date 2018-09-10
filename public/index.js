9c79b773a479613fb9cdec382158ff01c7c9e0d0
joshsarna <joshsarna@gmail.com>
2018-09-10 14:17:11 -0700

    fill first 36 cells in table with elements and make object, route, and template for show page

--- public/index.js
+++ public/index.js
@@ -1,10 +1,27 @@
 /* global Vue, VueRouter, axios */
 
+var ShowPage = {
+  template: "#show-page",
+  data: function() {
+    return {
+      message: "Periodic Table",
+      elements: []
+    };
+  },
+  created: function() {
+    axios.get("/api/elements").then(function(response) {
+      this.elements = response.data;
+    }.bind(this));
+  },
+  methods: {},
+  computed: {}
+};
+
 var HomePage = {
   template: "#home-page",
   data: function() {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Periodic Table",
       elements: []
     };
   },
@@ -18,7 +35,10 @@ var HomePage = {
 };
 
 var router = new VueRouter({
-  routes: [{ path: "/", component: HomePage }],
+  routes: [
+    { path: "/", component: HomePage },
+    { path: "/elements/:id", component: ShowPage }
+  ],
   scrollBehavior: function(to, from, savedPosition) {
     return { x: 0, y: 0 };
   }
